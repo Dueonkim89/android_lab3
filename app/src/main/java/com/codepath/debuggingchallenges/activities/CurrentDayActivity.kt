dbb489ee3f65547bc242c43ea6813652b3a63fc7
unknown <dueonkim89@Yahoo.com>
2022-03-08 12:08:33 -0800

    fixed first bug

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
@@ -3,22 +3,25 @@ package com.codepath.debuggingchallenges.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.codepath.debuggingchallenges.R
 import java.util.*
 
+private const val TAG = "CurrentDayActivity"
 class CurrentDayActivity : AppCompatActivity() {
     var tvDay: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_day)
-        tvDay = findViewById<View>(R.id.tvDay) as TextView
+        tvDay = findViewById<TextView>(R.id.tvDay)
         tvDay?.setText(dayOfMonth)
     }
 
-    private val dayOfMonth: Int
+    private val dayOfMonth: String
         get() {
             val cal = Calendar.getInstance()
-            return cal[Calendar.DAY_OF_MONTH]
+            // Log.i(TAG, "Day is ${cal[Calendar.DAY_OF_MONTH]}")
+            return cal[Calendar.DAY_OF_MONTH].toString()
         }
 }
